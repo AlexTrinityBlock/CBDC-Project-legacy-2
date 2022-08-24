e0f2559ca23bfc65021926b8719fb02bfff54970
AlexTrinityBlock <q123717111@gmail.com>
2022-08-25 04:38:56 +0800

    *

--- codes/bank/backend-server/laravel/routes/api.php
+++ codes/bank/backend-server/laravel/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Cache;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::get('/test1', function (){
+    Cache::put('key', 'Test', $seconds = 3600);
+    $value = Cache::get('key');
+    return $value ;
+});
+
+Route::get('/test2', function (){
+    $value = Cache::get('key');
+    return $value ;
 });
