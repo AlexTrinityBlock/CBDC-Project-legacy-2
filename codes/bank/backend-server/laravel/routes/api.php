008627006c3fcd13aa97df59f103976c6083ce43
AlexTrinityBlock <q123717111@gmail.com>
2022-08-24 16:02:56 +0800

    t

---
+++ codes/bank/backend-server/laravel/routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// 使用者訊息路由組
+Route::controller(UserController::class)->group(function () {
+    Route::get('/user', 'index');
+});
